aaca29732a877af3144eac6a2862d686df9371bf
Chichuyuu <paulragsac@gmail.com>
2025-02-03 11:14:10 +0800

    updated

--- src/xata.ts
+++ src/xata.ts
@@ -8,64 +8,293 @@ import type {
 
 const tables = [
   {
-    name: "Person",
+    name: "Users",
     columns: [
-      { name: "FirstName", type: "string" },
-      { name: "LastName", type: "string" },
+      {
+        name: "first_name",
+        type: "text",
+        notNull: true,
+        defaultValue: "first",
+      },
+      { name: "last_name", type: "text", notNull: true, defaultValue: "last" },
+      { name: "email", type: "text", notNull: true, defaultValue: "email.com" },
+      { name: "password_hash", type: "text" },
+      { name: "phone_number", type: "int", unique: true },
     ],
     revLinks: [
-      { column: "Person", table: "Loan" },
-      { column: "Person", table: "Savings" },
+      { column: "user", table: "SavingsRecords" },
+      { column: "user", table: "Expenses" },
+      { column: "user", table: "LoanPayments" },
+      { column: "user", table: "Goals" },
+      { column: "user", table: "Notifications" },
+      { column: "user", table: "Transactions" },
+      { column: "user", table: "Categories" },
+      { column: "user", table: "RecurringTransactionTemplates" },
+      { column: "user", table: "RecurringGoalContributions" },
     ],
   },
   {
-    name: "Loan",
+    name: "SavingsRecords",
     columns: [
-      { name: "LoanAmount", type: "int" },
-      { name: "InterestRate", type: "int" },
-      { name: "Description", type: "string" },
-      { name: "Person", type: "link", link: { table: "Person" } },
-      { name: "LoanName", type: "string" },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "text",
+      },
+      {
+        name: "is_recurring",
+        type: "bool",
+        notNull: true,
+        defaultValue: "false",
+      },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "date", type: "datetime", defaultValue: "now" },
     ],
-    revLinks: [{ column: "Loan", table: "PartitionedLoan" }],
   },
   {
     name: "Expenses",
     columns: [
-      { name: "expenseName", type: "string" },
-      { name: "expense", type: "int" },
-      { name: "expenseDate", type: "datetime" },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      { name: "date", type: "datetime", notNull: true, defaultValue: "now" },
+      {
+        name: "is_recurring",
+        type: "bool",
+        notNull: true,
+        defaultValue: "false",
+      },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "text",
+      },
+      {
+        name: "category",
+        type: "link",
+        link: { table: "Categories" },
+        unique: true,
+      },
     ],
   },
   {
-    name: "Income",
+    name: "LoanTypes",
     columns: [
-      { name: "incomeName", type: "string" },
-      { name: "income", type: "int" },
+      { name: "type", type: "text", notNull: true, defaultValue: "type" },
+    ],
+    revLinks: [{ column: "loan_type", table: "LoanPayments" }],
+  },
+  {
+    name: "LoanPayments",
+    columns: [
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      {
+        name: "start_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      {
+        name: "end_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      { name: "loan_type", type: "link", link: { table: "LoanTypes" } },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "text",
+      },
+    ],
+    revLinks: [{ column: "loan", table: "LoanInstallments" }],
+  },
+  {
+    name: "Goals",
+    columns: [
+      { name: "title", type: "text", notNull: true, defaultValue: "title" },
+      { name: "target_amount", type: "int", notNull: true, defaultValue: "0" },
+      { name: "saved_amount", type: "int", notNull: true, defaultValue: "0" },
+      { name: "image_url", type: "file[]" },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "text",
+      },
+      {
+        name: "due_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      {
+        name: "status",
+        type: "text",
+        notNull: true,
+        defaultValue: "in_progress",
+      },
+      { name: "priority_level", type: "int", notNull: true, defaultValue: "3" },
+      {
+        name: "goal_type",
+        type: "text",
+        notNull: true,
+        defaultValue: "short_term",
+      },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "category", type: "link", link: { table: "Categories" } },
+    ],
+    revLinks: [
+      { column: "goal", table: "Milestones" },
+      { column: "goal", table: "RecurringGoalContributions" },
     ],
   },
   {
-    name: "Goal",
+    name: "Milestones",
     columns: [
-      { name: "image", type: "file" },
-      { name: "name", type: "text", notNull: true, defaultValue: "GoalName" },
-      { name: "moneyRequired", type: "int" },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "text",
+      },
+      { name: "target_amount", type: "int", notNull: true, defaultValue: "0" },
+      {
+        name: "achieved_amount",
+        type: "int",
+        notNull: true,
+        defaultValue: "0",
+      },
+      {
+        name: "milestone_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      { name: "goal", type: "link", link: { table: "Goals" } },
     ],
   },
   {
-    name: "PartitionedLoan",
+    name: "Notifications",
     columns: [
-      { name: "Loan", type: "link", link: { table: "Loan" } },
-      { name: "PartitionedAmount", type: "int" },
-      { name: "DateDue", type: "datetime" },
-      { name: "isPaid", type: "bool" },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "message", type: "text", notNull: true, defaultValue: "message" },
+      { name: "is_read", type: "bool", notNull: true, defaultValue: "false" },
+      { name: "transaction", type: "link", link: { table: "Transactions" } },
+      {
+        name: "reminder_type",
+        type: "text",
+        notNull: true,
+        defaultValue: "goal_reached",
+      },
+      {
+        name: "notification_type",
+        type: "text",
+        notNull: true,
+        defaultValue: "general",
+      },
     ],
   },
   {
-    name: "Savings",
+    name: "Transactions",
     columns: [
-      { name: "currentAmount", type: "int", notNull: true, defaultValue: "0" },
-      { name: "Person", type: "link", link: { table: "Person" } },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      { name: "date", type: "datetime", notNull: true, defaultValue: "now" },
+      { name: "type", type: "text", notNull: true, defaultValue: "savings" },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "description",
+      },
+      { name: "category", type: "link", link: { table: "Categories" } },
+    ],
+    revLinks: [{ column: "transaction", table: "Notifications" }],
+  },
+  {
+    name: "Categories",
+    columns: [
+      { name: "name", type: "text", notNull: true, defaultValue: "name" },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "description",
+      },
+      { name: "purpose", type: "text", notNull: true, defaultValue: "expense" },
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "type", type: "text", notNull: true, defaultValue: "income" },
+    ],
+    revLinks: [
+      { column: "category", table: "Transactions" },
+      { column: "category", table: "Expenses" },
+      { column: "category", table: "Goals" },
+    ],
+  },
+  {
+    name: "RecurringGoalContributions",
+    columns: [
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "goal", type: "link", link: { table: "Goals" } },
+      {
+        name: "frequency",
+        type: "text",
+        notNull: true,
+        defaultValue: "weekly",
+      },
+      {
+        name: "next_execution_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      { name: "last_execution_date", type: "datetime" },
+      { name: "is_active", type: "bool", notNull: true, defaultValue: "true" },
+    ],
+  },
+  {
+    name: "LoanInstallments",
+    columns: [
+      { name: "loan", type: "link", link: { table: "LoanPayments" } },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      {
+        name: "payment_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+    ],
+  },
+  {
+    name: "RecurringTransactionTemplates",
+    columns: [
+      { name: "user", type: "link", link: { table: "Users" } },
+      { name: "amount", type: "int", notNull: true, defaultValue: "0" },
+      {
+        name: "frequency",
+        type: "text",
+        notNull: true,
+        defaultValue: "weekly",
+      },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        defaultValue: "description",
+      },
+      {
+        name: "next_execution_date",
+        type: "datetime",
+        notNull: true,
+        defaultValue: "now",
+      },
+      { name: "last_execution_date", type: "datetime" },
+      { name: "is_active", type: "bool", notNull: true, defaultValue: "true" },
     ],
   },
 ] as const;
@@ -73,35 +302,63 @@ const tables = [
 export type SchemaTables = typeof tables;
 export type InferredTypes = SchemaInference<SchemaTables>;
 
-export type Person = InferredTypes["Person"];
-export type PersonRecord = Person & XataRecord;
+export type Users = InferredTypes["Users"];
+export type UsersRecord = Users & XataRecord;
 
-export type Loan = InferredTypes["Loan"];
-export type LoanRecord = Loan & XataRecord;
+export type SavingsRecords = InferredTypes["SavingsRecords"];
+export type SavingsRecordsRecord = SavingsRecords & XataRecord;
 
 export type Expenses = InferredTypes["Expenses"];
 export type ExpensesRecord = Expenses & XataRecord;
 
-export type Income = InferredTypes["Income"];
-export type IncomeRecord = Income & XataRecord;
+export type LoanTypes = InferredTypes["LoanTypes"];
+export type LoanTypesRecord = LoanTypes & XataRecord;
 
-export type Goal = InferredTypes["Goal"];
-export type GoalRecord = Goal & XataRecord;
+export type LoanPayments = InferredTypes["LoanPayments"];
+export type LoanPaymentsRecord = LoanPayments & XataRecord;
 
-export type PartitionedLoan = InferredTypes["PartitionedLoan"];
-export type PartitionedLoanRecord = PartitionedLoan & XataRecord;
+export type Goals = InferredTypes["Goals"];
+export type GoalsRecord = Goals & XataRecord;
 
-export type Savings = InferredTypes["Savings"];
-export type SavingsRecord = Savings & XataRecord;
+export type Milestones = InferredTypes["Milestones"];
+export type MilestonesRecord = Milestones & XataRecord;
+
+export type Notifications = InferredTypes["Notifications"];
+export type NotificationsRecord = Notifications & XataRecord;
+
+export type Transactions = InferredTypes["Transactions"];
+export type TransactionsRecord = Transactions & XataRecord;
+
+export type Categories = InferredTypes["Categories"];
+export type CategoriesRecord = Categories & XataRecord;
+
+export type RecurringGoalContributions =
+  InferredTypes["RecurringGoalContributions"];
+export type RecurringGoalContributionsRecord = RecurringGoalContributions &
+  XataRecord;
+
+export type LoanInstallments = InferredTypes["LoanInstallments"];
+export type LoanInstallmentsRecord = LoanInstallments & XataRecord;
+
+export type RecurringTransactionTemplates =
+  InferredTypes["RecurringTransactionTemplates"];
+export type RecurringTransactionTemplatesRecord =
+  RecurringTransactionTemplates & XataRecord;
 
 export type DatabaseSchema = {
-  Person: PersonRecord;
-  Loan: LoanRecord;
+  Users: UsersRecord;
+  SavingsRecords: SavingsRecordsRecord;
   Expenses: ExpensesRecord;
-  Income: IncomeRecord;
-  Goal: GoalRecord;
-  PartitionedLoan: PartitionedLoanRecord;
-  Savings: SavingsRecord;
+  LoanTypes: LoanTypesRecord;
+  LoanPayments: LoanPaymentsRecord;
+  Goals: GoalsRecord;
+  Milestones: MilestonesRecord;
+  Notifications: NotificationsRecord;
+  Transactions: TransactionsRecord;
+  Categories: CategoriesRecord;
+  RecurringGoalContributions: RecurringGoalContributionsRecord;
+  LoanInstallments: LoanInstallmentsRecord;
+  RecurringTransactionTemplates: RecurringTransactionTemplatesRecord;
 };
 
 const DatabaseClient = buildClient();
